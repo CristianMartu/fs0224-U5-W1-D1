aec1edac9391c91988f86af41b96787a8ef425f8
CristianMartu <martuccicristian18@gmail.com>
2024-06-17 16:51:41 +0200

    some change

--- src/main/java/cristianmartucci/U5_W1_D1/entities/Pizza.java
+++ src/main/java/cristianmartucci/U5_W1_D1/entities/Pizza.java
@@ -14,14 +14,6 @@ public class Pizza {
     private int calories;
     private double price;
 
-//    @Override
-//    public String toString() {
-//        return "\n\t\t" + name +
-//                "\t toppings= " + toppings +
-//                "\t calories= " + calories +
-//                "\t price= " + price;
-//    }
-
     @Override
     public String toString(){
         StringBuilder result = new StringBuilder("\n\t\t" + name + " (");
@@ -29,7 +21,7 @@ public class Pizza {
             if (i < toppings.size() - 1) result.append(toppings.get(i).getName() + ", ");
             else result.append(toppings.get(i).getName());
         }
-        result.append(")\t\t" + calories + "\t\t" + price);
+        result.append(")\t\tcalories= " + calories + "\t\tprice= " + price);
     return result.toString();
     }
 }
